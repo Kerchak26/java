73193dc61e8ccbc180daead4b021c6d08a620767
Kerchak26 <124314219+Kerchak26@users.noreply.github.com>
2023-04-04 21:34:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+var numero = 1100;
+var zero = 0;
+var numeroNegativo = -250;
+var numeroReal = 25.65;
+var pi = 3.1415;
+var realNegativo = -19.13;
+var realZero = 10.0;
+
+console.log(numero, zero, numeroNegativo, numeroReal, pi, realNegativo, realZero)
+
+function returnGame(){
+    var game = 'Free Fire';
+    if(game ==='Free Fire'){
+        var game = 'Valorant';
+    console.log('Dentro do IF: ' + game);
+    }
+    console.log('Fora do IF: ' + game);
+}
+returnGame();
+
+function returnGameTwo(){
+    let game = 'Fortnite';
+    if(game ==='Fortnite'){
+        let game = 'GTA';
+        console.log('Dentro do IF: ' + game);
+    }
+    console.log('Fora do IF: ' + game);
+}
+returnGameTwo()
+const USER_ADMIN = {name:'Erick Ramos',
+login:'root'
+};
+USER_ADMIN.login='admin';// Alteramos o valor da propriedade Login
+console.log(USER_ADMIN);
+var n1 = parseInt(12);
+console.log(n1);
+var n1 = parseInt("Hello", 8);
+var n1 = parseFloat(3.14);
+console.log(n1);
+parseFloat("FF2");
+var n1 = parseInt(1);
+var n2 = parseInt(2);
+var soma = n1 + n2;
+console.log(soma)
+var n1 = parseFloat(5);
+var n2 = parseFloat(2);
+var subtração = n1 - n2;
+console.log(subtração)
+var n1 = parseFloat(10);
+var n2 = parseInt(2);
+var divisão = n1 / n2;
+console.log(divisão)
+var n1 = parseInt(10);
+var n2 = parseFloat(2);
+var multiplicação = n1 * n2;
+console.log(multiplicação)
+var n1 = parseInt(1280);
+var n2 = parseFloat(1280);
+var soma = n1 + n2;
+console.log(soma)
+var n1 = parseFloat(1299);
+var n2 = parseFloat(1288);
+var subtração = n1 - n2;
+console.log(subtração)
+var n1 = parseInt(1000);
+var n2 = parseInt(1001);
+var divisão = n1 / n2;
+console.log(divisão)
+var n1 = parseFloat(20);
+var n2 = parseFloat(1000000);
+var multiplicação = n1 * n2;
+console.log(multiplicação)
---
+++ script2.js
@@ -0,0 +1,12 @@
+function returnGameTwo(){
+    let game = 'Fortnite';
+    if(game ==='Fortnite'){
+        let game = 'GTA';
+        console.log('Dentro do IF: ' + game);
+    }
+    console.log('Fora do IF: ' + game);
+}
+returnGameTwo()
+const IP = '127.0.0.1';
+IP = '192.168.0.1';
+console.log(IP);
